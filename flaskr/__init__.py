c0ca7e79ebd1d041c36daa99509d5d1958a3a719
Janusz478 <JFeigel@mailbox.org>
2022-01-14 07:30:37 +0100

    added flaskr

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -33,4 +33,14 @@ def create_app(test_config=None):
     app.register_blueprint(page2)
     jwt.init_app(app)
 
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
